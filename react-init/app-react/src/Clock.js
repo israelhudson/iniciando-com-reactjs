62117af10d2ce5eebca713f4a41e6165b8a4ea0f
israelhudson <israelhudson@gmail.com>
2020-01-16 22:39:53 -0300

    adiconando relogio

---
+++ react-init/app-react/src/Clock.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+export default class Clock extends Component{
+    render(){
+        const returnClock = () => {
+            return new Date().toLocaleString('en-US', {
+                hour : 'numeric',
+                minute: 'numeric',
+                second: 'numeric',
+                hour12: true,
+            })
+        }
+        const color = 'red'
+        return (
+            <h1 className={color}>
+                {
+                    returnClock()
+                }
+            </h1>
+        );
+    }
+    
+}
